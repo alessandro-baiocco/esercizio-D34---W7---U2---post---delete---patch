087a361544894abe4c7787d6a7330cdd8dc1fbc8
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-08-24 17:16:19 +0200

    some fix

--- request.js
+++ request.js
@@ -1,9 +1,7 @@
 let loadImg1 = document.querySelector(".loadImg1");
-let postImg = document.querySelectorAll("rect");
-let hide = document.querySelectorAll(".nascondi");
+let postImg = document.querySelectorAll(".shadow-sm");
 let cards = document.querySelectorAll(".card");
-let noveMin = document.querySelectorAll(".text-muted");
-console.log(hide);
+
 fetch("https://api.pexels.com/v1/search?query=sky", {
   headers: {
     Authorization: "TbB35dMMKFN0zcMPt1bd7woZ8zTOXtzuULErgD4Em4NuKCvmUiigGwMs",
@@ -11,6 +9,25 @@ fetch("https://api.pexels.com/v1/search?query=sky", {
 })
   .then((stringedImages) => stringedImages.json())
   .then((images) => {
+    cards.forEach((dentroCard, index) => {
+      dentroCard.innerHTML = `<img src ="${images.photos[index].src.small}"
+    <div class="card-body">
+    <h5 class="card-title">${images.photos[index].alt}</h5>
+    <p class="card-text">
+    This is a wider card with supporting text below as a natural lead-in to additional content. This
+    content is a little bit longer.
+    </p>
+    <div class="d-flex justify-content-between align-items-center">
+    <div class="btn-group">
+    <button type="button" class="btn btn-sm btn-outline-secondary nascondi">View</button>
+    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+    </div>
+    <small class="text-muted">9 mins</small>
+    </div>
+    </div>`;
+    });
+    let noveMin = document.querySelectorAll(".text-muted");
+    let hide = document.querySelectorAll(".nascondi");
     hide.forEach((btn, index) => {
       btn.innerText = "hide";
       btn.addEventListener("click", () => {
